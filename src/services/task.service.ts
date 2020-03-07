b404c345cf5d091ab10a8524e2d96286afa20179
Adib Zidan <94amsz@gmail.com>
2020-03-07 23:41:06 +0100

    Add update and delete method

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { tasksMock } from '../mocks/tasks-mock';
 import { Task } from '../models/Task';
+import { deleteTask } from '../shared/delete-task';
 import { findTask } from '../shared/find-task';
 
 @Injectable()
@@ -19,6 +20,27 @@ export class TaskService {
       return `No task with the id of ${id} found!`;
     } else {
       return task;
+    }
+  }
+
+  public updateOne(id: string, task: Task): string | Task {
+    const taskToFind: Task = findTask(this.tasks, id);
+    const taskToUpdate: Task = Object.assign(taskToFind, task);
+
+    if (!task) {
+      return `No task with the id of ${id} found!`;
+    } else {
+      return taskToUpdate;
+    }
+  }
+
+  public deleteOne(id: string): string | Task[] {
+    const taskToDelete: Task[] = deleteTask(this.tasks, id);
+
+    if (!taskToDelete) {
+      return `No task with the id of ${id} found!`;
+    } else {
+      return taskToDelete;
     }
   }
 
